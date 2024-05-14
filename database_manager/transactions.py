46595c62ddd813149761aab6dfea0e4b2ee1f56e
Saee <saee_saadat@outlook.com>
2024-05-14 19:30:30 +0330

    added transaction id to transactions

--- database_manager/transactions.py
+++ database_manager/transactions.py
@@ -5,19 +5,21 @@ from database_manager import _get_connection_and_cursor
 from inventory.transactions import Transaction, SellTransaction, RestockTransaction, AddTransaction
 
 
-def save_transaction_to_database(transaction_type: str, product_id: int, quantity: int = 0, value: float = 0.0):
+def save_transaction_to_database(transaction_type: str, product_id: int, quantity: int = 0, value: float = 0.0) -> int:
     """
     This function will save the transaction to the database.
     :param transaction_type: Either `Add`, 'Sell' or 'Restock'
     :param product_id: the product that the transaction is about
     :param quantity: How many units of the product are involved in the transaction (0 for `Add`)
     :param value: How much money is involved in the transaction (0.0 for `Add`)
+    :return: ID of the new transaction
     """
     with _get_connection_and_cursor(commit=True) as (_, cursor):
         cursor.execute("INSERT INTO transactions (type, product_id, quantity, total_value) VALUES (?, ?, ?, ?)",
                        (transaction_type, product_id, quantity, value))
     logging.info("Saved new %s transaction with id #%d object for product #%d to the database",
                  transaction_type, cursor.lastrowid, product_id)
+    return cursor.lastrowid
 
 
 def get_all_product_transactions(product) -> List[Transaction]:
--- inventory/transactions.py
+++ inventory/transactions.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from database_manager import transactions as transaction_db
 
 
@@ -6,8 +8,9 @@ class Transaction:
     This class represents a transaction. It is an abstract class.
     """
 
-    def __init__(self, product):
+    def __init__(self, product, transaction_id: Optional[int] = None):
         self.product = product
+        self.transaction_id = transaction_id
 
     def save_to_database(self):
         raise Exception("Not Implemented in the subclass!")
@@ -18,8 +21,8 @@ class SellTransaction(Transaction):
     This class represents a sell transaction.
     """
 
-    def __init__(self, product, quantity: int, value: float):
-        super().__init__(product)
+    def __init__(self, product, quantity: int, value: float, transaction_id: Optional[int] = None):
+        super().__init__(product, transaction_id=transaction_id)
         self.quantity = quantity
         self.value = value
         # The reason we save the value in the database instead of calculating it everytime, is that a transaction might
@@ -29,8 +32,11 @@ class SellTransaction(Transaction):
         return f"Sell Transaction: {self.quantity} units of product #{self.product.product_id}"
 
     def save_to_database(self):
-        transaction_db.save_transaction_to_database('Sell', self.product.product_id, self.quantity,
-                                                    self.quantity * self.product.selling_price)
+        self.transaction_id = transaction_db.save_transaction_to_database('Sell',
+                                                                          self.product.product_id,
+                                                                          self.quantity,
+                                                                          self.quantity * self.product.selling_price
+                                                                          )
 
 
 class RestockTransaction(Transaction):
@@ -38,8 +44,8 @@ class RestockTransaction(Transaction):
     This class represents a restock transaction.
     """
 
-    def __init__(self, product, quantity: int, value: float):
-        super().__init__(product)
+    def __init__(self, product, quantity: int, value: float, transaction_id: Optional[int] = None):
+        super().__init__(product, transaction_id=transaction_id)
         self.quantity = quantity
         self.value = value
 
@@ -47,8 +53,11 @@ class RestockTransaction(Transaction):
         return f"Restock Transaction: {self.quantity} units of product #{self.product.product_id}"
 
     def save_to_database(self):
-        transaction_db.save_transaction_to_database('Restock', self.product.product_id, self.quantity,
-                                                    self.quantity * self.product.purchase_price)
+        self.transaction_id = transaction_db.save_transaction_to_database('Restock',
+                                                                          self.product.product_id,
+                                                                          self.quantity,
+                                                                          self.quantity * self.product.purchase_price
+                                                                          )
 
 
 class AddTransaction(Transaction):
@@ -56,11 +65,11 @@ class AddTransaction(Transaction):
     This class represents an add transaction.
     """
 
-    def __init__(self, product):
-        super().__init__(product)
+    def __init__(self, product, transaction_id: Optional[int] = None):
+        super().__init__(product, transaction_id=transaction_id)
 
     def __str__(self):
         return f"Add Transaction: Product {self.product.name} -> #{self.product.product_id}"
 
     def save_to_database(self):
-        transaction_db.save_transaction_to_database('Add', self.product.product_id)
+        self.transaction_id = transaction_db.save_transaction_to_database('Add', self.product.product_id)
